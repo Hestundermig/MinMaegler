d7049d52ef8c8f49953eae78b179a6fbe939bf8c
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-05-08 09:07:04 +0200

    react app lavet, og mapper sat op.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <BrowserRouter> */}
+      {/* <Routes> */}
+        {/* <Route path="/" element={< />}> */}
+          {/* <Route index element={<Home />} /> */}
+        {/* </Route> */}
+      {/* </Routes> */}
+    {/* </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
